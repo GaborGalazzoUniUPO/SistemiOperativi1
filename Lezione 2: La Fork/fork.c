73c7fa68a8d25bdfe31e2279bf7bd93434d1a625
Gabor Galazzo <gabor.galazzo@gmail.com>
2018-10-25 10:34:02 +0200

    ciclo 2

--- Lezione 2: La Fork/fork.c
+++ Lezione 2: La Fork/fork.c
@@ -2,12 +2,19 @@
 
 int main(int argc, char const *argv[])
 {
-    pid_t p = xfork(__FILE__, __LINE__);
-    if(p == 0){ //FIGLIO
+    for (int i = 0; i < 2; i++)
+    {
+        pid_t p = xfork(__FILE__, __LINE__);
+        if (p == 0)
+        { //FIGLIO
 
-        printf("Io sono %d figlio di %d, il principe dei Nani\n", getpid(), getppid());
-    }else{ //PADRE
-        printf("Io sono %d padre di %d, il re sotto la Montagna\n", getpid(), p);
+            printf("Io sono %d figlio %d-esimo di %d, il principe dei Nani\n", getpid(), i, getppid());
+        }
+        else
+        { //PADRE
+            printf("Io sono %d padre di %d e figlio di %d, il re sotto la Montagna\n", getpid(), p, getppid());
+        }
     }
     printf("FINITO!\n");
+    return (0);
 }
